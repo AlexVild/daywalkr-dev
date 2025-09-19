e60f5968b9129c1dd709bde579c9d4893eb5d626
Alex Vild <ajv2324@gmail.com>
2025-09-19 11:06:24 -0400

    add analytics for funsies testing purposes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import ArchivePage from "./pages/ArchivePage";
 import PostView from "./pages/PostPage";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import { Analytics } from "@vercel/analytics/nuxt";
 
 function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <div className="app">
         <Header />
         <Routes>
